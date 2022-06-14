76625176e18cac778119adf7bfeed789fd4bd456
Francisca Estefanía Martínez Lozano <fany.marloz@gmail.com>
2022-06-14 15:07:05 -0500

    Pokedex atomic

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por Estefania ML <a href="https://gitlab.com/EstefaniaML">gitlab: EstefaniaML</a>
+     </div>
+  </footer>
+);
+
+export default Footer;
